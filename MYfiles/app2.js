5a8d27e5cbd119c0d3a752cb923f71b4e967f608
Teresa-Maingi <114757041+Teresa-Maingi@users.noreply.github.com>
2023-04-22 01:00:11 +0300

    Add files via upload

---
+++ MYfiles/app2.js
@@ -0,0 +1,7 @@
+function showSignupForm() {
+    document.querySelector('.signup-form').style.display = 'block';
+  }
+  function hideSignupForm() {
+    document.querySelector('.signup-form').style.display = 'none';
+    
+  }
